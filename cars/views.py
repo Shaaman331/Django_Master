7cb6d2f8c350e1f091bb27ad1047e409d10d76e5
Tarcisio <tarcisio2010souza@gmail,com>
2024-09-20 10:10:04 -0300

    11-Commit Criacao da variavel search retorna objeto GET search com informacao do usuario, buscando modelo na variavel car.

--- cars/views.py
+++ cars/views.py
@@ -4,10 +4,10 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    print(request.GET)  # retorna o objeto GET request com informacao do usuario
+    search = request.GET.get('search')  # retorna o objeto GET search com informacao do usuario
 
 
-    cars = Car.objects.filter(model__contains = 'Gol') #buscar modelos do objeto Car
+    cars = Car.objects.filter(model__contains = search) #buscar modelos do objeto Car search 
     
     return render(
         request,
