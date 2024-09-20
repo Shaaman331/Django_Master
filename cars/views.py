05cc754372820c9da2ca221790ee3154a5a682ed
Tarcisio <tarcisio2010souza@gmail,com>
2024-09-19 22:27:29 -0300

    09-Commit ORM Renderizar template na views e passar objeto para o templete.

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 # Create your views here.
 def cars_view(request):
+    cars = Car.objects.all() #buscar modelos do objeto Car
+    
     return render(
         request,
-        'cars.html',
-        {'cars': {'model': 'Jeep Renagate 1.6'}}
+        'cars.html', # Renderiza  o template
+        {'cars': cars}  # Passa o objeto cars para o template
 
         ) 
