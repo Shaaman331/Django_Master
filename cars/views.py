92e81ad1afef58e14ba43ac8c496c0a1ed5f4ba5
Tarcisio <tarcisio2010souza@gmail,com>
2024-09-20 09:48:55 -0300

    10-Commit Objeto request.Get retorna informacao do usuario

--- cars/views.py
+++ cars/views.py
@@ -4,7 +4,10 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all() #buscar modelos do objeto Car
+    print(request.GET)  # retorna o objeto GET request com informacao do usuario
+
+
+    cars = Car.objects.filter(model__contains = 'Gol') #buscar modelos do objeto Car
     
     return render(
         request,
