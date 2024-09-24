8dddd7676773d25cace2f1262a0e1a286d7f0467
Tarcisio <tarcisio2010souza@gmail,com>
2024-09-24 14:10:18 -0300

    15-Commit Criando rota "new_car" , nova funcao para "new_car_view", metodos para salvar dados no formulário.

--- cars/forms.py
+++ cars/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 #Criando Form usuário de Cadastro de marca
 
-class CarForm(forms.Form):
+class CarForm(forms.Form): 
     model = forms.CharField(max_length=200)
     brand = forms.ModelChoiceField(Brand.objects.all())
     factory_year = forms.IntegerField()
@@ -11,7 +11,21 @@ class CarForm(forms.Form):
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
     photo = forms.ImageField()
-    
+
+    def save(self):  #Método para salvar os dados do formulário
+
+        car = Car(   #Criando um objeto Car
+            model = self.cleaned_data['model'], # Pegando os dados do formulário
+            brand = self.cleaned_data['brand'],  
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photo = self.cleaned_data['photo'],
+        )
+        car.save() # Salva o objeto no banco de dados
+
+        return car
 
 
 
