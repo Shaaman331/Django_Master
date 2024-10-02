cf9f04e69306d75d9d879014442284603bd3ced5
Tarcisio <tarcisio2010souza@gmail,com>
2024-10-02 20:04:39 -0300

    18-Commit Criando "method POST" validando e salvando usuario, criando Form username, password e erros.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,14 @@
-from django.contrib.auth.forms import  UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
+def register_view(request): #Funcao definida para lidar com requisicoes de registro de usuario
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)  # Cria um formulário com os dados enviados
+        if user_form.is_valid():  # Verifica se o formulário é válido
+            user_form.save()  # Salva o formulário
+            return redirect('car_list')  # Redireciona para a página de car_list
+    else:
+        user_form = UserCreationForm()  # Instancia o UserCreationForm para o método GET
 
-def register_view(request):  #Funcao definida para lidar com requisicoes de registro de usuario
-    user_form =  UserCreationForm()   #Instancia a classe UserCreationForm para coletar informacoes do usuario
-    return render (request, 'register.html', {'user_form': user_form}) #Renderiza a view com o formulario de registro
-
-
+    return render(request, 'register.html', {'user_form': user_form})   
+ 
