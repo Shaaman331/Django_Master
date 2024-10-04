1d0325f4c9cbc72ea343db89117d1c0c14b4b864
Tarcisio <tarcisio2010souza@gmail,com>
2024-10-03 21:03:19 -0300

    18-Commit Criacao do "login_view", "logout_view" e integracao com usuario.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 def register_view(request): #Funcao definida para lidar com requisicoes de registro de usuario
@@ -6,9 +7,31 @@ def register_view(request): #Funcao definida para lidar com requisicoes de regis
         user_form = UserCreationForm(request.POST)  # Cria um formulário com os dados enviados
         if user_form.is_valid():  # Verifica se o formulário é válido
             user_form.save()  # Salva o formulário
-            return redirect('car_list')  # Redireciona para a página de car_list
+            return redirect('login')  # Redireciona para a página de car_list
     else:
         user_form = UserCreationForm()  # Instancia o UserCreationForm para o método GET
 
     return render(request, 'register.html', {'user_form': user_form})   
  
+def login_view(request): # Função para lidar com requisições de login de usuário
+    if request.method == 'POST': #  Verifica se a requisição é POST, verifica se o usuario enviou um formulario com os dados de login
+        username = request.POST['username']  # Se o método for "POST" pega o nome de usuario do formulario eviado
+        password = request.POST['password']   # Se o método for "POST" pega a senha do formulario eviado
+        user = authenticate(request, username=username, password=password) #Autentica o usuário usando as credenciais fornecidas, caso valída retornao o objeto User, caso contrário retorna None.
+
+        if user is not None: #  Verifica se o usuário foi autenticado com sucesso
+            login(request, user)  # Faz o login do usuário e armazena em ambas as secoes
+            return redirect('cars_list') # Redireciona o usario para "cars_list"  caso o login seja bem sucedido
+
+        else:  # Caso o login não seja bem sucedido
+
+            login_form = AuthenticationForm()  # Instanciar o formulário corretamente
+    else:
+        login_form = AuthenticationForm()
+
+    return render(request, 'login.html', {'login_form': login_form})  # Renderiza a página de login com o formulário de login preenchido
+
+def logout_view(request): # Função para lidar com requisições de logout de usuário
+    logout(request)   # Chama a funcao  logout do django para fazer o logout do usuario 
+    return redirect('cars_list') # Redireciona o usuário para a página de car_list
+
--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,16 @@ from django.urls import path
 from django.conf  import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
-from accounts.views import register_view
+from accounts.views import register_view, login_view, logout_view
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'), # Criando rota para acesso 'register_view)'
+    path('login/',login_view, name =  'login'),  # Criando rota para acesso 'login_view'
+    path('logout/',logout_view,  name =  'logout'),
+
     path('cars/', cars_view, name= 'cars_list'), # Criando rota para acesso 'car list'
     path('new_car/', new_car_view, name= 'new_car'),  # Criando rota para acesso 'new car'
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
